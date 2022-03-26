2b9e81e3220d77f6c36ff7dcc1fb8345b9ecf823
Evgenii <osipenkov.evgenii@gmail.com>
2022-03-26 16:37:20 +0300

    Add prod public path to the Vue config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service');
 
+const { REPO_NAME = '/' } = process.env;
+
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' ? REPO_NAME : '/',
   transpileDependencies: true,
 });
